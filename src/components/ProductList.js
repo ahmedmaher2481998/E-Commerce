26f4fbaba26fb858ab6d93599df9a66dae1f5c26
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-06-10 13:42:02 +0200

    wroking on the list & grid view /35/

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,8 +4,14 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-	const { filtered_products: products } = useFilterContext();
-	return <GridView products={products}>product list</GridView>;
+	const { filtered_products: products, gridView } = useFilterContext();
+	return products.length < 1 ? (
+		<h5 style={{ textTransform: "none" }}>
+			Sorry, no products match yor search...
+		</h5>
+	) : gridView ? (
+		<GridView products={products}>product list</GridView>
+	): <ListView
 };
 
 export default ProductList;
