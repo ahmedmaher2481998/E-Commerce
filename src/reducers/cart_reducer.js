27318d094b9ea8842e17d2db16b08ec9fe38fd75
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-06-26 01:36:26 +0200

    add to cart fun&action is done

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -10,6 +10,22 @@ const cart_reducer = (state, { type, payload }) => {
 	const newState = { ...state };
 	switch (type) {
 		case ADD_TO_CART:
+			const { id, color, amount, product } = payload;
+			let tempItme = newState.cart.find((i) => i.id == id + color);
+			if (tempItme) {
+				alert("needs to be implemented ");
+			} else {
+				const newItem = {
+					id: id + color,
+					name: product.name,
+					color,
+					amount,
+					price: product.price,
+					max: product.stock,
+					image: product.images[0].url,
+				};
+				newState.cart.push(newItem);
+			}
 			return newState;
 	}
 	throw new Error(`No Matching "${action.type}" - action type`);
