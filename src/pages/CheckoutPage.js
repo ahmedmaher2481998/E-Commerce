f6de9db59bed8f1cc24f6da37c29d8ea871ceed4
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-07-10 22:52:27 +0200

    finalize

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -9,12 +9,26 @@ import { useCartContext } from "../context/cart_context";
 import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
+	const { cart } = useCartContext();
 	return (
-		<>
-			<StripeCheckout />
-		</>
+		<main>
+			<PageHero title='checkout' />
+			<Wrapper className='page'>
+				{cart.length < 1 ? (
+					<div className='empty'>
+						<h2>your cart is empty</h2>
+						<Link to='/products' className='btn'>
+							fill it
+						</Link>
+					</div>
+				) : (
+					<StripeCheckout />
+				)}
+			</Wrapper>
+		</main>
 	);
 };
+
 //eslint-disable-next-line
 const Wrapper = styled.div``;
 export default CheckoutPage;
